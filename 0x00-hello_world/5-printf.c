cd3af425a6b5f86e223c6e02b9150758f788501e
Alan-ST <alan.gon138383@gmail.com>
2021-06-17 14:20:58 -0500

    0x00 tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
- * main - prints exactly "with proper grammar, but the outcome is a piece of art", followed by a new line.
+ * main: prints exactly "with proper grammar, but the outcome is a piece of art", followed by a new line.
  * Return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /*
- * main - prints the size of various types on the computer it is compiled and run on.
+ * main: prints the size of various types on the computer it is compiled and run on.
  * Return: 0
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
