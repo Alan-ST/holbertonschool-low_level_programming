ab154a4b6c10f865db558678379389ca42c5a37f
Alan-ST <alan.gon138383@gmail.com>
2021-06-18 20:47:15 -0500

    0x01 tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets 
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
