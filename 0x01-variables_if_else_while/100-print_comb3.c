2df715ed65e5511f14678126bdd2ce5188bdd626
Alan-ST <alan.gon138383@gmail.com>
2021-06-18 21:28:14 -0500

    0x01 tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/*
+* main - prints a comb
+* Return: 0
+*/
+int main(void)
+{
+	int x = '0';
+
+	while  (x <= '9')
+	{
+		int y = '0';
+
+		while  (y <= '9')
+		{
+			if (x != y)
+			{
+				if (x < y)
+				{
+					putchar(x);
+					putchar(y);
+					if (x == '8' && y == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y += 1;
+		}
+		x += 1;
+	}
+	putchar('\n');
+	return (0);
+}
