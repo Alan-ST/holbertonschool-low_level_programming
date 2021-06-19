07c3b2455200d6bdba8ad013257dcebcf19ffa88
Alan-ST <alan.gon138383@gmail.com>
2021-06-18 21:07:34 -0500

    0x01 tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+	
+	for (l = 'z'; l >= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
