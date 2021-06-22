e73544817aeca7204b92a03e8eb5e0f18c459b6f
Alan-ST <alan.gon138383@gmail.com>
2021-06-21 19:49:30 -0500

    0x02 tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar (x / 10 + '0');
+			_putchar (x % 10 + '0');
+			_putchar (':');
+			_putchar (y / 10 + '0');
+			_putchar (y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
