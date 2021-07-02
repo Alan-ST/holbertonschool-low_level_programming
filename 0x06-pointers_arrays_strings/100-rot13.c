06cb8ab151d8c3bd85acc3112242adf78625b9f1
Alan-ST <alan.gon138383@gmail.com>
2021-07-02 11:20:30 -0500

    0x06 tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; abc[j] != '\0'; j++)
+		{
+			if (s[i] == abc[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
