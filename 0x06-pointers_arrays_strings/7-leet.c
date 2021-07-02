b7434aadc24e024354cd4357f30e5925ca06fd3a
Alan-ST <alan.gon138383@gmail.com>
2021-07-02 11:30:51 -0500

    0x06 tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,15 @@
 char *leet(char *s)
 {
 	int i, j;
-	char abc[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
-	char leet[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char a[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
+	char l[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; abc[j] != '\0'; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == abc[j])
-				s[i] = leet[j];
+			if (s[i] == a[j])
+				s[i] = l[j];
 		}
 	}
 	return (s);
