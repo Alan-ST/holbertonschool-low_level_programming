f36cbc89163ad3896b3f3222af79732b483ef313
Alan-ST <alan.gon138383@gmail.com>
2021-07-02 10:59:21 -0500

    0x06 tasks

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string1
+ * @s2: string2
+ * Return: diff
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0; diff = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	if (s1[i] != s2[i])
+		diff = s1[i] - s2[i];
+	return (diff);
+}
