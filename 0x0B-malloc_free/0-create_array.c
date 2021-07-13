3caddec46697bbb61e0a4a1382052a369fb65ea5
Alan-ST <alan.gon138383@gmail.com>
2021-07-13 15:34:47 -0500

    0x0B tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * create_array - creates an array of chars
+ * @size: size
+ * @c: char
+ * Return: 0 or an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(sizeof(char) * size);
+	if (size == 0 || ar == 0)
+		return (0);
+	else
+	{
+		for (i = 0; i < size; i++)
+			ar[i] = c;
+		return (ar);
+	}
+}
