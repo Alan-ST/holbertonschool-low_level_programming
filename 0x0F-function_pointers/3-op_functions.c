eb2f03532caed150141182946b3ae1439f99abbb
Alan-ST <alan.gon138383@gmail.com>
2021-07-23 18:38:39 -0500

    0x0F tasks

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - add two numbers
+ * @a: num
+ * @b: num
+ * Return: sum
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract two numbers
+ * @a: num
+ * @b: num
+ * Return: diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulates two numbers
+ * @a: num
+ * @b: num
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
