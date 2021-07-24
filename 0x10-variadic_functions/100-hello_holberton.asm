64e3ea98b896dab042ef3449592980f49311847b
Alan-ST <alan.gon138383@gmail.com>
2021-07-24 11:57:59 -0500

    0x10 tasks

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
-global main
-section .data
-	msg db 10,"Hello, Holberton"
-	l equ $-msg
-section .text
-main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, l
-	syscall
-	mov rax, 60
-	mov rdi, 0
-	syscall
+global main				;moving to _start
+section .data                           ;.data starts
+	msg db 10,"Hello, Holberton"	;string initialized
+	l equ $-msg			;string's length
+section .text				;.text starts
+main:					;_start label
+	mov rax, 1			;sys_write function
+	mov rdi, 1			;std_out file descriptor
+	mov rsi, msg			;offset of msg
+	mov rdx, l			;msg's length
+	syscall				;call the Kernel
+
+	mov rax, 60			;sys_exit function
+	mov rdi, 0			;successful termination
+	syscall				;call The Kernel
