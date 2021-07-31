639bc92863d50c216c5dccf1735cf4b76475fcf6
Alan-ST <alan.gon138383@gmail.com>
2021-07-30 20:22:44 -0500

    0x12 tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: list
+ * Return: list's length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
