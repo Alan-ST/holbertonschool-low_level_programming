7477f8e0d5fe32619f75f6c6e3e499c6fcfb6be9
Alan-ST <alan.gon138383@gmail.com>
2021-07-30 20:08:07 -0500

    0x12 tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,6 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *l = h;
-	int i = 0; 
 	size_t n = 0;
 
 	while (l != NULL)
@@ -18,7 +17,6 @@ size_t print_list(const list_t *h)
 			printf("[%lu] %s\n", l.len, l.str);
 		l = l.next;
 		n++;
-		i++;
 	}
 	return (n);
 }
