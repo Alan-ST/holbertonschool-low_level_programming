614ab32abf831fe0e3727041ef1c90fee58fc923
Alan-ST <alan.gon138383@gmail.com>
2021-07-31 18:44:04 -0500

    0x12 tasks

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: list
+ */
+void free_list(list_t *head)
+{
+	list_t *h, *l;
+
+	h = head;
+	while (h != NULL)
+	{
+		l = h->next;
+		free(h->str);
+		free(h);
+		h = l;
+	}
+}
