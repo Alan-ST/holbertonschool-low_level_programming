a939452797fccbd39f52f4f763fb5bb4dda523b2
Alan-ST <alan.gon138383@gmail.com>
2021-08-03 18:32:21 -0500

    0x13 tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *head = h;
 	size_t i = 0;
 
-	while (head != NULL)
+	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n", head->n);
-		head = head->next;
-		i++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (i);
 }
