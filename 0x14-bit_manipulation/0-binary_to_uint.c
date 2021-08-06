4228f151e926e7f5d036846238e4d7803cd8f4ab
Alan-ST <alan.gon138383@gmail.com>
2021-08-06 00:24:51 -0500

    0x14 tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, val = 0, bin = 0, j = 0;
 
-	if (b == NULL)
+	if (b == NULL || b[0] == '\0')
 		return (0);
 	while (b[i] != '\0')
 	{
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 	while (j <= i)
 	{
 		bin = (b[j] - '0') << (i - j);
-		val += bin;
+		val = bin + val;
 		j++;
 	}
 	return (val);
