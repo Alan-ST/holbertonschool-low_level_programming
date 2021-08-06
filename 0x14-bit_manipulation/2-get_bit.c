082a76829935c39d8afa4b25d27734f8a567bf74
Alan-ST <alan.gon138383@gmail.com>
2021-08-06 09:59:05 -0500

    0x14 tasks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned int) * sizeof(long))
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 	else
 		return ((n >> index) & 1);
