a38e0cd840303b1fb259dcc7da16490437126fb9
Alan-ST <alan.gon138383@gmail.com>
2021-08-10 05:26:16 -0500

    0x15 tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	OP = open(filename, O_RDONLY);
-	if (op == EOF)
+	if (OP == EOF)
 		return (0);
 	s = malloc(sizeof(char) * letters);
 	if (s == NULL)
