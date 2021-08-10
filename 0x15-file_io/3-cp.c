2a685ce8d6b81607b27e62bcfe52eaa813a7aeec
Alan-ST <alan.gon138383@gmail.com>
2021-08-10 06:04:43 -0500

    0x15 tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,31 +7,31 @@
  */
 int main(int argc, char **argv)
 {
-	int file, newfile;
+	int file_from, file_to;
 	ssize_t RD, WR;
 	char buffer[BUFF];
 
 	if (argc != 3)
     {
-        dprintf(STDERR_FILENO, "Usage: cp <file> <newfile>\n");
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
-	file = open(argv[1], O_RDONLY);
-	if (file == EOF)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == EOF)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
-	newfile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (newfile == EOF)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == EOF)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
     }
     while (RD == BUFF)
     {
-        RD = read(file, buffer, BUFF);
-		WR = write(newfile, buffer, RD);
+        RD = read(file_from, buffer, BUFF);
+		WR = write(file_to, buffer, RD);
     }
 	if (RD == EOF)
     {
@@ -43,14 +43,14 @@ int main(int argc, char **argv)
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
     }
-	if (close(file) == EOF)
+	if (close(file_from) == EOF)
 	{
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
         exit(100);
     }
-	if (close(newfile) == EOF)
+	if (close(file_to) == EOF)
     {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", newfile);
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
         exit(100);
     }
 	return (0);
