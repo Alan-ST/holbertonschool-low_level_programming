56411191d022d0318487014dcf5fc9611fd1d45f
Alan-ST <alan.gon138383@gmail.com>
2021-08-27 17:56:32 -0500

    0x09 tasks

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - main function
+ * @c: char
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - main function
+ * @src: source
+ * @dest: destiny
+ * Return: The pointer to destination buffer.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int r = 0;
+
+	while (dest[i])
+		i++;
+	while (src[r])
+	{
+		dest[i] = src[r];
+		i++;
+		r++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
